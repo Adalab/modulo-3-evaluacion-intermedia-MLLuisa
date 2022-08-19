d84d8057b3214e44a6439dbf605a8f12d37533ca
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-08-19 10:21:53 +0200

    add functionality to add new phrase and new character

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
 import '../styles/App.scss';
 import data from '../services/api';
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 
 
 function App() {
 
-const [friendsPhrase, setFriendsPhrase] = useState([]);
+const [friendsPhrase, setFriendsPhrase] = useState(data);
+const [newPhrase, setNewPhrase] = useState("");
+const [newCharacter, setNewCharacter] = useState("");
 
-useEffect(() => {
-  setFriendsPhrase(data)
-});
+const saveNewPhrase = (ev) => {
+  setNewPhrase(ev.target.value);
+}
+
+const saveNewCharacter = (ev) => {
+  setNewCharacter(ev.target.value);
+}
+
+const addNewPhraseandCharacter = (ev) => {
+  ev.preventDefault();
+  const newList = {quote: newPhrase, character: newCharacter};
+  setFriendsPhrase([...friendsPhrase, newList])
+  setNewCharacter("");
+  setNewPhrase("");
+}
 
 const renderPhrases = () => {
-  return data.map((phrase, index
+  return friendsPhrase.map((phrase, index
     ) => {
     return <li key={index}>{phrase.quote} - {phrase.character}</li>
   })
@@ -22,7 +36,27 @@ const renderPhrases = () => {
     <div className="App">
       <h1>Frases de Friends</h1>
       <ul>{renderPhrases()}</ul>
+      <h3>Añadir una nueva frase</h3>
+      <form>
+      <label>
+    Frase:
+    <input type="text" 
+    name="name"
+    value={newPhrase} 
+    onChange={saveNewPhrase}/>
+  </label>
+  <label>
+    Personaje:
+    <input type="text" 
+    name="name" 
+    value={newCharacter}
+    onChange={saveNewCharacter}/>
+  </label>
+      </form>
+      <button 
+      onClick={addNewPhraseandCharacter}>Añadir una nueva frase</button>
     </div>
+
   );
 }
 
