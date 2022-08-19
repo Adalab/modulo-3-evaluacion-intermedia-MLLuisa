d5fe6491b929f4b592d41a021671d1e3351d6f69
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-08-19 09:24:18 +0200

    add firends phrases to the page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import '../styles/App.scss';
+import data from '../services/api';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+const [friendsPhrase, setFriendsPhrase] = useState([]);
+
+useEffect(() => {
+  setFriendsPhrase(data)
+});
+
+const renderPhrases = () => {
+  return data.map((phrase, index
+    ) => {
+    return <li key={index}>{phrase.quote} - {phrase.character}</li>
+  })
+}
+
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <h1>Frases de Friends</h1>
+      <ul>{renderPhrases()}</ul>
     </div>
   );
 }
